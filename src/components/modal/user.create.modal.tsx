d06c824c861ea48560e4cd09128af9588576765c
quangan010903@gmail.com <quangan010903@gmail.com>
2025-07-22 21:41:15 +0700

    create user

--- src/components/modal/user.create.modal.tsx
+++ src/components/modal/user.create.modal.tsx
@@ -2,13 +2,17 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../redux/hook';
+import { createNewUser, resetCreate } from '../../redux/user/user.slice';
+import { toast } from 'react-toastify';
 
 const UserCreateModal = (props: any) => {
     const { isOpenCreateModal, setIsOpenCreateModal } = props;
-
+    const dispatch = useAppDispatch();
     const [email, setEmail] = useState<string>("");
     const [name, setName] = useState<string>("");
+    const isCreateSuccess = useAppSelector(state => state.user.isCreateSuccess)
 
     const handleSubmit = () => {
         if (!email) {
@@ -19,8 +23,17 @@ const UserCreateModal = (props: any) => {
             alert("name empty");
             return;
         }
-        console.log(">>> check create: ", { email, name })
+        dispatch(createNewUser({ email, name }))
     }
+    useEffect(() => {
+        if (isCreateSuccess === true) {
+            setIsOpenCreateModal(false)
+            toast('Wow so easy! Create succeed')
+            setEmail("")
+            setName("")
+            dispatch(resetCreate())
+        }
+    }, [isCreateSuccess])
 
     return (
         <>
--- src/redux/user/user.slice.ts
+++ src/redux/user/user.slice.ts
@@ -2,35 +2,64 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 export const fetchListUsers = createAsyncThunk(
   'users/fetchListUser',
-  async (userId, thunkAPI) => {
-     const res = await fetch('http://localhost:8000/users')
-     const data = await res.json();
-     return data;
+  async () => {
+    const res = await fetch('http://localhost:8000/users')
+    const data = await res.json();
+    return data;
   }
 )
-interface IUser {
-  id : number;
-  name : string;
-  email : string
+
+export const createNewUser = createAsyncThunk(
+  'users/createNewUser',
+  async (payload: IUserPayload, thunkAPI) => {
+    const res = await fetch('http://localhost:8000/users', {
+      method: "POST",
+      body: JSON.stringify({ email: payload.email, name: payload.name }),
+      headers: { "Content-Type": "application/json" }
+    });
+    const data = await res.json();
+    if (data && data.id) {
+      thunkAPI.dispatch(fetchListUsers());
+    }
+    return data;
+  }
+)
+interface IUserPayload {
+  email: string,
+  name: string
 }
-const initialState : { listUsers : IUser[] } = {
-  listUsers : [],
+interface IUser {
+  id: number;
+  name: string;
+  email: string
+}
+const initialState:
+  {
+    listUsers: IUser[]
+    isCreateSuccess: boolean
+  } = {
+  listUsers: [],
+  isCreateSuccess: false
 }
 
 export const UserSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
+    resetCreate(state){
+       state.isCreateSuccess = false;
+    }
   },
   extraReducers: (builder) => {
-    // Add reducers for additional action types here, and handle loading state as needed
     builder.addCase(fetchListUsers.fulfilled, (state, action) => {
-      // Add user to the state array
-       state.listUsers = action.payload;
-    })
+      state.listUsers = action.payload;
+    }),
+      builder.addCase(createNewUser.fulfilled, (state) => {
+        state.isCreateSuccess = true;
+      })
   },
 })
 
-export const {  } = UserSlice.actions
+export const { resetCreate } = UserSlice.actions
 
 export default UserSlice.reducer
